1214cee798448bbafb335ae193e742b202c7c35c
rkjepso <38100136+rkjepso@users.noreply.github.com>
2022-03-25 12:08:53 +0100

    .

--- Client/CarViewModel.cs
+++ Client/CarViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -28,17 +29,19 @@ public record CarViewModel
 
     public static CarViewModel CreateRandomCar()
     {
-        string[] f = { "Volvo", "Volvo", "Ferrari", "Porche", "Jaguar" };
-        string[] l = { "Amazon", "PV", "512", "911", "EType" };
+        string[] b = { "Volvo", "Volvo", "Ferrari", "Porche", "Jaguar" };
+        string[] m = { "Amazon", "PV", "512", "911", "EType" };
         (int, int)[] yr = { (1965, 1970), (1949, 1965), (1980, 1990), (1950, 2011), (1965, 1970) };
         (int, int)[] p = { (100, 200), (20, 250), (700, 3000), (500, 3500), (400, 2000) };
+        
+        Debug.Assert((b.Length, b.Length, b.Length)==(m.Length, yr.Length, p.Length), "Error Length");
 
         CarViewModel car = new();
         Random r = new();
 
-        int i = r.Next(0, f.Length);
-        car.Brand = f[i];
-        car.Model = l[i];
+        int i = r.Next(0, b.Length);
+        car.Brand = b[i];
+        car.Model = m[i];
         car.Year = r.Next(yr[i].Item1, yr[i].Item2);
         car.Price = r.Next(p[i].Item1, p[i].Item2) * 1000;
 
@@ -51,7 +54,7 @@ public record CarViewModel
             return "";
         return (car.Brand, car.Model) switch
         {
-            ("Volvo", "Amazn") => "Img/VolvoAmazon.jpg",
+            ("Volvo", "Amazon") => "Img/VolvoAmazon.jpg",
             ("Volvo", "PV") => "Img/VolvoPV.jpg",
             ("Ferrari", "512") => "Img/Ferrari512.jpg",
             ("Porche", "911") => "Img/Porche911.jpg",
